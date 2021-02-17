1563c9d1921612e342b208b6b2eaed70710f4c28
Athapa116 <adathapa116@gmail.com>
2021-02-16 20:56:08 -0500

    Adding today's practice session's script

---
+++ Practice session R script.R
@@ -0,0 +1,15 @@
+#Reading the data in R
+##filename<- read_csv("../Desktop/filename.csv")
+
+## filename <- > names(banjade3)[names(banjade3)== "X2"] <- "SN"
+## View(banjade3)
+##table(banjade3$X7)
+  
+##For exclusing all the NA from the data
+  #banjade2 <- banjade[!(is.na(banjade$X3)),]
+   #View(banjade2)
+  # banjade3 <- banjade2[!(is.na(banjade2$X4)),]
+   #View(banjade3)
+##To check the table for a specific col
+##table(banjade3$X7) 
+
